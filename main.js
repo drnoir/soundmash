aa039acb46ae64560133002c88ef258485aa2c96
Chris Godber <chrispgodber@gmail.com>
2017-06-10 18:26:23 +0000

    initital commit
    setting up html, js and css

---
+++ main.js
@@ -0,0 +1,28 @@
+var soundFiles= ['glitch1.wav','glitch2.wav','glitch3.wav']; 
+var initShapeNum = 2;
+var maxshapeNum  = 15;
+
+//init initital generative objects 
+var initShapeObjects = [
+	$(".shape"),
+	$(".shape2")
+]
+
+function getSound(soundFiles){
+var currSoundFile = soundFiles.pop();
+	}
+	
+function SoundMash(){
+//divide time of sound into 4 slices , array??
+
+}	
+	
+
+function refresh(){
+	
+	}
+
+function generate shapes(){
+	//clone and randomise some element of css
+	
+}
